1cf36d97014539e5cd3742205b9ee88d128958d5
Nguyen Ba Ky <nguyenbaky23@gmail.com>
2024-11-11 17:29:14 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Switch, Route, Link, NavLink } from "react-router-dom";
 import ColorBox from "./components/ColorBox";
 import Counter from "./components/Counter";
 import AlbumFeature from "./features/Album/pages";
@@ -8,7 +9,24 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <TodoFeature></TodoFeature>
+      <h1>Home</h1>
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/albums">albums</Link>
+      </p>
+      <p>
+        <NavLink to="/todos">Todos</NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums">albums</NavLink>
+      </p>
+      <Routes>
+        <Route path="/todos" Component={TodoFeature} />
+        <Route path="/albums" Component={AlbumFeature} />
+      </Routes>
+
       {/* <AlbumFeature></AlbumFeature>
       {/* <ColorBox></ColorBox>
       <Counter /> */}
